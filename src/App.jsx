c4de698bf7ee271b958faeb226031863fa7d6780
Dev-Sarmad <sarmadh230@gmail.com>
2025-10-06 19:06:08 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+//when we using the gsap plugins we have to register them first
+gsap.registerPlugin(ScrollTrigger);
+function App() {
+  return <div className="">App</div>;
+}
+
+export default App;
